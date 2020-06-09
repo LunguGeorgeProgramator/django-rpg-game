c7210b8d2ca83e8a597c2c586d94a9e921318081
LunguGeorgeProgramator <51510106+LunguGeorgeProgramator@users.noreply.github.com>
2020-06-09 21:25:18 +0300

    work on user interface main on login page

--- player/urls.py
+++ player/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile', views.index, name='show_profile'),
+    path('profile', views.index, name='show_plyers'),
+    path('profile/<int:id>', views.show, name='show_profile'),
     path('profile/create', views.create, name='create_profile'),
+    path('profile/login', views.login, name='login_profile'),
 ]
--- player/views.py
+++ player/views.py
@@ -1,22 +1,73 @@
 from django.shortcuts import render, redirect
 from player.forms import RegisterPlayerForm, LoginPlayerForm
+from django.core.exceptions import ObjectDoesNotExist
 from player.models import Player, Skills
 
 def index(request):
-    return render(request, 'player/index.html')
+    players = Player.objects.all()
+    return render(request, 'player/index.html', { 'players': players })
 
 def create(request):
     if request.method == 'POST':
         create_form_player = RegisterPlayerForm(request.POST)
-        login_form_player = LoginPlayerForm(request.POST)
-        if login_form_player.is_valid():
-            return redirect('show_profile')
+        if create_form_player.is_valid():
+            return store(request)
     else:
         create_form_player = RegisterPlayerForm(request.GET or None)
-        login_form_player = LoginPlayerForm(request.GET or None)
-
     return render(request, 'player/create.html',{
-        'create_form_player': create_form_player,
-        'login_form_player': login_form_player,
-        'method': 'creare'
+        'create_form_player': create_form_player
     })
+
+def store(request):
+    return redirect('show_profile')
+
+def edit(request):
+    return
+
+def update(request):
+    return
+
+def login(request):
+    player = None
+    error = ''
+    if request.method == 'POST':
+        login_form_player = LoginPlayerForm(request.POST)
+        if login_form_player.is_valid():
+            user_name = login_form_player.cleaned_data.get('nume_utilizator')
+            player = Player.objects.filter(
+                nume_utilizator = user_name, 
+                email = login_form_player.cleaned_data.get('email')
+            )
+            if player:
+                return show(request, player.id) 
+            else:
+                error = 'Nu exista userul ' +  user_name
+    else:
+        login_form_player = LoginPlayerForm(request.GET or None)
+    return render(request, 'player/login.html',{
+        'login_form_player': login_form_player,
+        'error': error 
+    })
+
+
+def show(request, id):
+    try:
+        player = Player.objects.get(id=id)
+    except ObjectDoesNotExist:
+        player = {
+            'nume': 'XXX',
+            'prenume': 'XXX',
+            'email': 'XXX@XXX.XXX',
+            'nume_utilizator': 'XXX',
+            'level': 'XXX',
+            'experienta': 'XXX',
+            'attack': 'XXX',
+            'defense': 'XXX',
+            'health': 'XXX',
+            'mana': 'XXX',
+            'luck': 'XXX'
+        }
+    return render(request, 'player/show.html', { 'player': player })
+
+def delete(request):
+    return
